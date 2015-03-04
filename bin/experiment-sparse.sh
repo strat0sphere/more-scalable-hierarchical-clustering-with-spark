032b1f051c35aff08fd81989438a5891a70df61c
Yuu ISHIKAWA <yuu.ishikawa@gmail.com>
2015-03-04 10:37:40 +0900

    Add test for SparseVector

---
+++ bin/experiment-sparse.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+## ENV
+source ~/spark/conf/spark-env.sh
+SPARK_SUBMIT=${HOME}/spark/bin/spark-submit
+__SPARK_MASTER="spark://${SPARK_MASTER_IP}:7077"
+
+__JAR="${HOME}/hierarchical-clustering-with-spark/target/scala-2.10/hierarchical-clustering_2.10-0.0.1.jar"
+
+## paramters
+__MAX_CPU_CORES_LIST="160"
+__DATA_SIZE_LIST="1000000"
+__DIMENSION_LIST="10"
+__NUM_CLUSTERS_LIST="127"
+__SPARSITY_LIST="0.1"
+
+for __DATA_SIZE in $__DATA_SIZE_LIST
+do
+  for __DIMENSION in $__DIMENSION_LIST
+  do
+    for __NUM_CLUSTERS in $__NUM_CLUSTERS_LIST
+    do
+      for __SPARSITY_LIST in $__SPARSITY
+      do
+        for __MAX_CPU_CORES in $__MAX_CPU_CORES_LIST
+        do
+          __NUM_PARTITIONS=$(($__MAX_CPU_CORES * 1))
+          $SPARK_SUBMIT  \
+            --driver-memory 16g \
+            --master "$__SPARK_MASTER" \
+            --class HierarchicalClusteringSparseApp \
+            --total-executor-cores $__MAX_CPU_CORES \
+            $__JAR \
+            "$__SPARK_MASTER" $__MAX_CPU_CORES $__DATA_SIZE $__DIMENSION $__NUM_CLUSTERS $__NUM_PARTITIONS $__SPARSITY
+        done
+      done
+    done
+  done
+done
+
